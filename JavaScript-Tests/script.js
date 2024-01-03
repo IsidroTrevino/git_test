cefc8431560b2bd912b1e8234bc03b5f969e7fad
IsidroTrevino <A00837104@tec.mx>
2024-01-02 21:30:41 -0700

    Added some more excercises

--- JavaScript-Tests/script.js
+++ JavaScript-Tests/script.js
@@ -1,29 +1,24 @@
-const container = document.querySelector('#container');
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
-container.appendChild(content); // Append content to container
+button.addEventListener('click', () => {
+  const myItem = input.value;
+  input.value = '';
 
-const para = document.createElement('p');
-para.textContent = "Hey I'm red!";
-para.style.color = 'red';
-container.appendChild(para); // Append para to container
+  const listItem = document.createElement('li');
+  const listText = document.createElement('span');
+  const listBtn = document.createElement('button');
 
-const h3 = document.createElement('h3');
-h3.textContent = "I'm a blue h3!";
-h3.style.color = 'blue';
-container.appendChild(h3); // Append h3 to container
+  listItem.appendChild(listText);
+  listText.textContent = myItem;
+  listItem.appendChild(listBtn);
+  listBtn.textContent = 'Delete';
+  list.appendChild(listItem);
 
-const div = document.createElement('div');
-div.style.border = '1px solid black';
-div.style.backgroundColor = 'pink';
-container.appendChild(div); // Append div to container
+  listBtn.addEventListener('click', () => {
+    list.removeChild(listItem);
+  });
 
-const h1 = document.createElement('h1');
-h1.textContent = "I'm in a div";
-div.appendChild(h1); // Append h1 to div
-
-const p = document.createElement('p');
-p.textContent = "ME TOO!";
-div.appendChild(p); // Append p to div
+  input.focus();
+});
